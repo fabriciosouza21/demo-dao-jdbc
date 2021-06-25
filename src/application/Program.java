fb1053194c61c24215834552702a59c02644c10e
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-06-25 16:39:18 -0300

    saller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department departamento = new Department(1,"computacao");
+		Seller seller = new Seller(1, "fabricio", "fabriciosouza21777@gmail", new Date(),3000.0 , departamento);
 		System.out.println(departamento);
+		System.out.println(seller);
 
 	}
 
