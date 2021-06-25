86fcebd41cb36790a6c9bbc75d1a629e8eb455d4
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-06-25 15:51:45 -0300

    Department entities class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department departamento = new Department(1,"computacao");
+		System.out.println(departamento);
+
+	}
+
+}
